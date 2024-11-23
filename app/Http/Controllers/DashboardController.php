6a233a2a85116b1385f96a2e6962d383add036c3
Jacky Zhang <jacky.chang123456789@gmail.com>
2024-11-23 16:46:39 +0700

    dashboard initial

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Warehouse;
+use App\Models\MaterialRequest;
+use App\Models\DeliveryOrder;
 
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        return view('home');
+        $user = auth()->user();
+        if ($user && $user->hasRole('Super Admin')) {
+            $totalWarehouses = Warehouse::count(); 
+            $totalMaterialRequest = MaterialRequest::count();
+            $totalDeliveryNote = DeliveryOrder::count();
+        } else {
+            $totalWarehouses = $user->warehouses()->count();
+            $totalMaterialRequest = MaterialRequest::whereIn('warehouse_id', $user->warehouses()->pluck('id'))->count();
+            $totalDeliveryNote = DeliveryOrder::whereIn('warehouse_id', $user->warehouses()->pluck('id'))->count();
+        }
+
+        return view('home', compact('totalWarehouses', 'totalMaterialRequest', 'totalDeliveryNote'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,18 @@
 @stop
 
 @section('content')
-<p>Welcome to this beautiful admin panel.</p>
+
+<div class="row">
+    <div class="col-lg-3 col-md-3 col-sm-12">
+        <x-adminlte-info-box title="Total Gudang" text="{{$totalWarehouses}}" icon="fas fa-lg fa-warehouse" icon-theme="purple"/>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12">
+        <x-adminlte-info-box title="Total SPM" text="{{$totalMaterialRequest}}" icon="fas fa-lg fa-file-signature" icon-theme="primary"/>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-12">
+        <x-adminlte-info-box title="Total Surat Jalan" text="{{$totalDeliveryNote}}" icon="fas fa-lg fa-truck" icon-theme="warning"/>
+    </div>
+</div>
 @stop
 
 @section('css')
