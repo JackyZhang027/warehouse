54a315dba73c4940def5cf1bfb2f227e5c2df0af
Jacky Zhang <jacky.chang123456789@gmail.com>
2025-07-23 20:45:34 +0700

    stock qty

--- resources/views/search/track_item.blade.php
+++ resources/views/search/track_item.blade.php
@@ -114,7 +114,7 @@
                     </tr>
                 @empty
                     <tr>
-                        <td colspan="3" class="text-center">Tidak ada data</td>
+                        <td colspan="4" class="text-center">Tidak ada data</td>
                     </tr>
                 @endforelse
             </tbody>
